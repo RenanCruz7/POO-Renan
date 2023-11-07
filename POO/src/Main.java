d31b83945f6dac8df6576708c094d1ecd774ca28
RenanCruz <renancruz199@gmail.com>
2023-11-06 23:48:56 -0300

    Get e Set e alguns testes

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,19 +1,37 @@
 class Personagem{
     String nome;
-    /*
     String cor;
     int quantidadeDeCogumelos;
     float altura;
     String tipoFisico;
     boolean possuiBigode;
-     **/
-    Personagem(String nome) {
+
+    Personagem() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Personagem p1 = new Personagem("Matheus");
+        Personagem p1 = new Personagem();
+        p1.setNome("Renan");
+
         System.out.println(p1.nome);
+
+        Quadrilatero q1 = new Quadrilatero(5);
+        Quadrilatero q2 = new Quadrilatero(5);
+
+        System.out.println(q1.equals(q2));
     }
 }
