81554e6cda1a8536b1960a0424a069a41f28a945
RenanCruz <renancruz199@gmail.com>
2023-11-10 00:07:53 -0300

    Alguns testes

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -20,18 +20,60 @@ class Personagem{
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public void setQuantidadeDeCogumelos(int cogumelos){this.quantidadeDeCogumelos = cogumelos;}
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setPossuiBigode(boolean possuiBigode) {
+        this.possuiBigode = possuiBigode;
+    }
+
+    public void setTipoFisico(String tipoFisico) {
+        this.tipoFisico = tipoFisico;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Personagem p1 = new Personagem();
+        Personagem p2 = new Personagem();
+
         p1.setNome("Renan");
+        p1.setCor("Vermelho");
+        p1.setAltura(1.8F);
+        p1.setPossuiBigode(false);
+        p1.setTipoFisico("Magro");
 
         System.out.println(p1.nome);
+        System.out.println(p1.cor);
+        System.out.println(p1.altura);
+        System.out.println(p1.possuiBigode);
+        System.out.println(p1.tipoFisico);
 
-        Quadrilatero q1 = new Quadrilatero(5);
-        Quadrilatero q2 = new Quadrilatero(5);
-
-        System.out.println(q1.equals(q2));
+        System.out.println(p1.hashCode());
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.toString());
     }
 }
