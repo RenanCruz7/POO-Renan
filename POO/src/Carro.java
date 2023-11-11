fc8341cd094f1846d48aa650bbf16740b6f67efe
RenanCruz <renancruz199@gmail.com>
2023-11-10 22:40:58 -0300

    Heranças

---
+++ POO/src/Carro.java
@@ -0,0 +1,38 @@
+public class Carro {
+    String marca;
+    Float preço;
+    String nomeCarro;
+    String dono;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Float getPreço() {
+        return preço;
+    }
+
+    public void setPreço(Float preço) {
+        this.preço = preço;
+    }
+
+    public String getNomeCarro() {
+        return nomeCarro;
+    }
+
+    public void setNomeCarro(String nomeCarro) {
+        this.nomeCarro = nomeCarro;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+}
---
+++ POO/src/Funcionario.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class Funcionario extends Pessoa {
+
+    Date dataAdmimissao;
+    String matricula;
+
+    public Funcionario(String nome, String sexo, String matricula) {
+        super(nome, sexo);
+        this.matricula = matricula;
+    }
+
+    public Date getDataAdmimissao() {
+        return dataAdmimissao;
+    }
+
+    public void setDataAdmimissao(Date dataAdmimissao) {
+        this.dataAdmimissao = dataAdmimissao;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
