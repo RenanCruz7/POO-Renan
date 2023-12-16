173b83f2f1778ac66aceeac36c78b7b3e6c06734
RenanCruz <renancruz199@gmail.com>
2023-12-16 12:32:36 -0300

    Interface

---
+++ POO/src/ContasGoverno.java
@@ -0,0 +1,10 @@
+public interface ContasGoverno {
+    double	disponibilizarFaturamentoMensal();
+
+    double	disponibilizarFaturamentoAnual();
+}
+
+/*
+* Neste caso utilizo uma interface que define o que o hospital deve enviar para o governo para prestar as contas
+* e na classe Transmissão eu implemento como isso será feito
+* */
---
+++ POO/src/TransmissaoContasGoverno.java
@@ -0,0 +1,12 @@
+public class TransmissaoContasGoverno implements ContasGoverno{
+
+    @Override
+    public double disponibilizarFaturamentoMensal() {
+        return 0;
+    }
+
+    @Override
+    public double disponibilizarFaturamentoAnual() {
+        return 0;
+    }
+}
